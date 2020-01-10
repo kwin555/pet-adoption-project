451235e817185124431dd57f0b83424f727ea63b
kwin555 <kevftw595@gmail.com>
2020-01-09 22:15:42 -0600

    added hooks

---
+++ src/SearchParams.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropDown from "./useDropDown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropDown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropDown("Breed", "", breeds);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
