175cabef3489e4f2670bd25b6d3d012bfba53f0a
Kevin T Nguyen <knguyen03@pros.com>
2020-01-14 09:31:48 -0600

    added context for theme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 import { Router, Link } from "@reach/router";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState({
+    buttonColor: "darkblue",
+    modalColo: "pink"
+  });
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
+      <ThemeContext.Provider value = {themeHook} >
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
 
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+      
     </React.StrictMode>
   );
 };
